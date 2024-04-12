313cf809d9b00de60f6201b063ee9314e7c1b17e
j-ishaan007 <165316021+j-ishaan007@users.noreply.github.com>
2024-04-12 15:19:01 +0000

    changes saved

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+// opetraions
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2="Ishaan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ 01_basics/05_conditionals.js
@@ -0,0 +1,27 @@
+// operators
+
+
+// console.log(2>1)
+// console.log(2>=1)
+
+console.log("2">1);
+console.log("02">1);
+// to compare two entities the dataypes of the two must be same, typescript does not allow the following operation
+
+console.log(null>0);
+console.log(null==0);
+console.log(null >= 0);
+
+// comparisons converts null into a number zero!!
+
+console.log(undefined == 0);
+
+// ===, strict check, checks the datatype of the variable too
+console.log("2" === 2);
+
+const id = Symbol('1234')
+const anotherid = Symbol('1234')
+console.log(id === anotherid);
+
+const bignumber = 23445588899999n
+console.log(typeof(bignumber));
